f2f8bbec78b5bcb0ae8b0d3af25a04ecadce33eb
Alex Paduraru <axelut@me.com>
2015-08-14 18:30:00 +0300

    finished profile page, finished table page, start working on typography page

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -105,20 +105,26 @@ function initDemoChartist(){
     var dataSales = {
       labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
       series: [
-        [0, 285, 120, 90, 70, 54, 44, 32, 22, 11, 4, 2, 2],
-        [0, 100, 48, 40, 28, 23, 15, 10, 6, 3, 1, 1, 0],
+         [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
+        [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
+        [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
       ]
     };
     
     var optionsSales = {
       lineSmooth: false,
       low: 0,
-      high: 400,
+      high: 800,
       showArea: true,
       height: "245px",
       axisX: {
-        showGrid: false
-      }
+        showGrid: false,
+      },
+      lineSmooth: Chartist.Interpolation.simple({
+        divisor: 3
+      }),
+      showLine: false,
+      showPoint: false,
     };
 
     Chartist.Line('#chartHours', dataSales, optionsSales);
