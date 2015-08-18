d120a2d8828920c7d3f5244d4d67a96bf356073a
Alex Paduraru <axelut@me.com>
2015-08-18 22:21:07 +0300

    curatat fissure + sass + javascripturi, redenumit fissure, ready for archive 1.0 product

--- assets/js/bootstrap-checkbox-radio-switch.js
+++ assets/js/bootstrap-checkbox-radio-switch.js
@@ -1,3 +1,256 @@
+!function ($) {
+
+ /* CHECKBOX PUBLIC CLASS DEFINITION
+  * ============================== */
+
+  var Checkbox = function (element, options) {
+    this.init(element, options);
+  }
+
+  Checkbox.prototype = {
+    
+    constructor: Checkbox
+    
+  , init: function (element, options) {      
+    var $el = this.$element = $(element)
+    
+    this.options = $.extend({}, $.fn.checkbox.defaults, options);      
+    $el.before(this.options.template);    
+    this.setState(); 
+  }  
+   
+  , setState: function () {    
+      var $el = this.$element
+        , $parent = $el.closest('.checkbox');
+        
+        $el.prop('disabled') && $parent.addClass('disabled');   
+        $el.prop('checked') && $parent.addClass('checked');
+    }  
+    
+  , toggle: function () {    
+      var ch = 'checked'
+        , $el = this.$element
+        , $parent = $el.closest('.checkbox')
+        , checked = $el.prop(ch)
+        , e = $.Event('toggle')
+      
+      if ($el.prop('disabled') == false) {
+        $parent.toggleClass(ch) && checked ? $el.removeAttr(ch) : $el.prop(ch, ch);
+        $el.trigger(e).trigger('change'); 
+      }
+    }  
+    
+  , setCheck: function (option) {    
+      var d = 'disabled'
+        , ch = 'checked'
+        , $el = this.$element
+        , $parent = $el.closest('.checkbox')
+        , checkAction = option == 'check' ? true : false
+        , e = $.Event(option)
+      
+      $parent[checkAction ? 'addClass' : 'removeClass' ](ch) && checkAction ? $el.prop(ch, ch) : $el.removeAttr(ch);
+      $el.trigger(e).trigger('change');       
+    }  
+      
+  }
+
+
+ /* CHECKBOX PLUGIN DEFINITION
+  * ======================== */
+
+  var old = $.fn.checkbox
+
+  $.fn.checkbox = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('checkbox')
+        , options = $.extend({}, $.fn.checkbox.defaults, $this.data(), typeof option == 'object' && option);
+      if (!data) $this.data('checkbox', (data = new Checkbox(this, options)));
+      if (option == 'toggle') data.toggle()
+      if (option == 'check' || option == 'uncheck') data.setCheck(option)
+      else if (option) data.setState(); 
+    });
+  }
+  
+  $.fn.checkbox.defaults = {
+    template: '<span class="icons"><span class="first-icon fa fa-square-o"></span><span class="second-icon fa fa-check-square-o"></span></span>'
+  }
+
+
+ /* CHECKBOX NO CONFLICT
+  * ================== */
+
+  $.fn.checkbox.noConflict = function () {
+    $.fn.checkbox = old;
+    return this;
+  }
+
+
+ /* CHECKBOX DATA-API
+  * =============== */
+
+  $(document).on('click.checkbox.data-api', '[data-toggle^=checkbox], .checkbox', function (e) {
+    var $checkbox = $(e.target);
+    if (e.target.tagName != "A") {      
+      e && e.preventDefault() && e.stopPropagation();
+      if (!$checkbox.hasClass('checkbox')) $checkbox = $checkbox.closest('.checkbox');
+      $checkbox.find(':checkbox').checkbox('toggle');
+    }
+  });
+  
+  $(function () {
+    $('[data-toggle="checkbox"]').each(function () {
+      var $checkbox = $(this);
+      $checkbox.checkbox();
+    });
+  });
+
+}(window.jQuery);
+
+
+
+/* =============================================================
+ * flatui-radio v0.0.3
+ * ============================================================ */
+
+!function ($) {
+
+ /* RADIO PUBLIC CLASS DEFINITION
+  * ============================== */
+
+  var Radio = function (element, options) {
+    this.init(element, options);
+  }
+
+  Radio.prototype = {
+  
+    constructor: Radio
+    
+  , init: function (element, options) {      
+      var $el = this.$element = $(element)
+      
+      this.options = $.extend({}, $.fn.radio.defaults, options);      
+      $el.before(this.options.template);    
+      this.setState();
+    }   
+    
+  , setState: function () {    
+      var $el = this.$element
+        , $parent = $el.closest('.radio');
+        
+        $el.prop('disabled') && $parent.addClass('disabled');   
+        $el.prop('checked') && $parent.addClass('checked');
+    } 
+    
+  , toggle: function () {    
+      var d = 'disabled'
+        , ch = 'checked'
+        , $el = this.$element
+        , checked = $el.prop(ch)
+        , $parent = $el.closest('.radio')      
+        , $parentWrap = $el.closest('form').length ? $el.closest('form') : $el.closest('body')
+        , $elemGroup = $parentWrap.find(':radio[name="' + $el.attr('name') + '"]')
+        , e = $.Event('toggle')
+      
+        if ($el.prop(d) == false) {
+            $elemGroup.not($el).each(function () {
+              var $el = $(this)
+                , $parent = $(this).closest('.radio');
+                
+                if ($el.prop(d) == false) {
+                  $parent.removeClass(ch) && $el.removeAttr(ch).trigger('change');
+                } 
+            });
+            
+            if (checked == false) $parent.addClass(ch) && $el.prop(ch, true);
+            $el.trigger(e);
+          
+            if (checked !== $el.prop(ch)) {
+                $el.trigger('change'); 
+            }
+        }               
+    } 
+     
+  , setCheck: function (option) {    
+      var ch = 'checked'
+        , $el = this.$element
+        , $parent = $el.closest('.radio')
+        , checkAction = option == 'check' ? true : false
+        , checked = $el.prop(ch)
+        , $parentWrap = $el.closest('form').length ? $el.closest('form') : $el.closest('body')
+        , $elemGroup = $parentWrap.find(':radio[name="' + $el['attr']('name') + '"]')
+        , e = $.Event(option)
+        
+      $elemGroup.not($el).each(function () {
+        var $el = $(this)
+          , $parent = $(this).closest('.radio');
+          
+          $parent.removeClass(ch) && $el.removeAttr(ch);
+      });
+            
+      $parent[checkAction ? 'addClass' : 'removeClass'](ch) && checkAction ? $el.prop(ch, ch) : $el.removeAttr(ch);
+      $el.trigger(e);  
+          
+      if (checked !== $el.prop(ch)) {
+        $el.trigger('change'); 
+      }
+    }  
+     
+  }
+
+
+ /* RADIO PLUGIN DEFINITION
+  * ======================== */
+
+  var old = $.fn.radio
+
+  $.fn.radio = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('radio')
+        , options = $.extend({}, $.fn.radio.defaults, $this.data(), typeof option == 'object' && option);
+      if (!data) $this.data('radio', (data = new Radio(this, options)));
+      if (option == 'toggle') data.toggle()
+      if (option == 'check' || option == 'uncheck') data.setCheck(option)
+      else if (option) data.setState(); 
+    });
+  }
+  
+  $.fn.radio.defaults = {
+    template: '<span class="icons"><span class="first-icon fa fa-circle-o"></span><span class="second-icon fa fa-dot-circle-o"></span></span>'
+  }
+
+
+ /* RADIO NO CONFLICT
+  * ================== */
+
+  $.fn.radio.noConflict = function () {
+    $.fn.radio = old;
+    return this;
+  }
+
+
+ /* RADIO DATA-API
+  * =============== */
+
+  $(document).on('click.radio.data-api', '[data-toggle^=radio], .radio', function (e) {
+    var $radio = $(e.target);
+    e && e.preventDefault() && e.stopPropagation();
+    if (!$radio.hasClass('radio')) $radio = $radio.closest('.radio');
+    $radio.find(':radio').radio('toggle');
+  });
+  
+  $(function () {
+    $('[data-toggle="radio"]').each(function () {
+      var $radio = $(this);
+      $radio.radio();
+    });
+  });
+
+}(window.jQuery);
+
+
+
 /* ============================================================
  * bootstrapSwitch v1.3 by Larentis Mattia @spiritualGuru
  * http://www.larentis.eu/switch/
--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,216 +1,142 @@
-function initMorphingButtons(){
-     //   simulate morphing buttons for demo purpose
+type = ['','info','success','warning','danger'];
+    	
+
+demo = {
+    initPickColor: function(){
+        $('.pick-class-label').click(function(){
+            var new_class = $(this).attr('new-class');  
+            var old_class = $('#display-buttons').attr('data-class');
+            var display_div = $('#display-buttons');
+            if(display_div.length) {
+            var display_buttons = display_div.find('.btn');
+            display_buttons.removeClass(old_class);
+            display_buttons.addClass(new_class);
+            display_div.attr('data-class', new_class);
+            }
+        });
+    },
     
-    $('#successBtn').click(function(){
-       var $button = $(this);
-                 
-        // simulate a Success Ajax call for 2 seconds         
-        setTimeout(function(){
-            $button.morphingButton({
-                action: 'setState',
-                icon: 'fa-check',
-                state: 'success'
-            });        
-        }, 2000);
-    });
-    
-     $('#errorBtn').click(function(){
-        var $button = $(this);
+    initChartist: function(){    
         
-        // simulate a Error Ajax call for 2 seconds          
-        setTimeout(function(){
-            $button.morphingButton({
-                action: 'setState',
-                icon: 'fa-times',
-                state: 'error'
-            });        
-        }, 2000);
-    });
+        var dataSales = {
+          labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
+          series: [
+             [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
+            [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
+            [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
+          ]
+        };
+        
+        var optionsSales = {
+          lineSmooth: false,
+          low: 0,
+          high: 800,
+          showArea: true,
+          height: "245px",
+          axisX: {
+            showGrid: false,
+          },
+          lineSmooth: Chartist.Interpolation.simple({
+            divisor: 3
+          }),
+          showLine: false,
+          showPoint: false,
+        };
     
-    $('#starBtn').click(function(){
-        var $button = $(this);
+        Chartist.Line('#chartHours', dataSales, optionsSales);
+        
     
-        $button.morphingButton({
-            action: 'setState',
-            icon: 'fa-star',
-            state: 'warning'
-        });        
-     
-    });
+        var data = {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
+            [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
+          ]
+        };
+        
+        var options = {
+            seriesBarDistance: 10,
+            axisX: {
+                showGrid: false
+            },
+            height: "245px"
+        };
+        
+        var responsiveOptions = [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+        
+        Chartist.Bar('#chartActivity', data, options, responsiveOptions);
     
-    $('#thumbBtn').click(function(){
-        var $button = $(this);
-        setTimeout(function(){
-            $button.morphingButton({
-                action: 'setState',
-                icon: 'fa-thumbs-up',
-                state: 'info'
-            });        
-        }, 2000);
-    });
-}
-
-function initDemoChartist(){
-    var dataPerformance = {
-      labels: ['9pm', '2am', '8am', '2pm', '8pm'],
-      series: [
-        [1, 6, 8, 7, 4, 7, 8, 12, 16, 17, 14, 13]
-      ]
-    };
+        var dataPreferences = {
+            series: [
+                [25, 30, 20, 25]
+            ]
+        };
+        
+        var optionsPreferences = {
+            donut: true,
+            donutWidth: 40,
+            startAngle: 0,
+            total: 100,
+            showLabel: false,
+            axisX: {
+                showGrid: false
+            }
+        };
     
-    var optionsPerformance = {
-      showPoint: false,
-      lineSmooth: true,
-      axisX: {
-        showGrid: false,
-        showLabel: true
-      },
-      axisY: {
-        offset: 40,
-      },
-      low: 0,
-      high: 20
-    };
-
-    //Chartist.Line('#chartPerformance', dataPerformance, optionsPerformance);
+        Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+        
+        Chartist.Pie('#chartPreferences', {
+          labels: ['62%','32%','6%'],
+          series: [62, 32, 6]
+        });   
+    },
     
-    var dataStock = {
-      labels: ['2009', '2010', '2011', '2012', '2013', '2014'],
-      series: [
-        [22.20, 28.16, 34.90, 42.28, 47.26, 48.89, 51.93, 55.32, 59.21, 62.21, 75.50, 80.23, 60.32, 55.03, 62.21, 78.83, 82.12, 89.21, 102.50, 107.23]
-      ]
-    };
+    initGoogleMaps: function(){
+        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
+        var mapOptions = {
+          zoom: 13,
+          center: myLatlng,
+          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+          styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
     
-    var optionsStock = {
-      lineSmooth: false,
-      axisY: {
-        offset: 40,
-        labelInterpolationFnc: function(value) {
-            return '$' + value;
-          }
-
-      },
-      low: 10,
-      high: 110,
-       classNames: {
-          point: 'ct-point ct-green',
-          line: 'ct-line ct-green'
-      }
-    };
-     
-    var $chart = $('#chartStock');
-    
-    //Chartist.Line('#chartStock', dataStock, optionsStock);     
-    
-    var dataSales = {
-      labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-      series: [
-         [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-        [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-        [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
-      ]
-    };
-    
-    var optionsSales = {
-      lineSmooth: false,
-      low: 0,
-      high: 800,
-      showArea: true,
-      height: "245px",
-      axisX: {
-        showGrid: false,
-      },
-      lineSmooth: Chartist.Interpolation.simple({
-        divisor: 3
-      }),
-      showLine: false,
-      showPoint: false,
-    };
-
-    Chartist.Line('#chartHours', dataSales, optionsSales);
-    
-
-    var data = {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      series: [
-        [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-        [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-      ]
-    };
-    
-    var options = {
-        seriesBarDistance: 10,
-        axisX: {
-            showGrid: false
-        },
-        height: "245px"
-    };
-    
-    var responsiveOptions = [
-      ['screen and (max-width: 640px)', {
-        seriesBarDistance: 5,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value[0];
-          }
         }
-      }]
-    ];
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            title:"Hello World!"
+        });
+        
+        // To add the marker to the map, call setMap();
+        marker.setMap(map);
+    },
     
-    Chartist.Bar('#chartActivity', data, options, responsiveOptions);
+	showNotification: function(from, align){
+    	color = Math.floor((Math.random() * 4) + 1);
+    	
+    	$.notify({
+        	icon: "pe-7s-gift",
+        	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+        	
+        },{
+            type: type[color],
+            timer: 4000,
+            placement: {
+                from: from,
+                align: align
+            }
+        });
+	}
 
-    var dataViews = {
-      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      series: [
-        [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-      ]
-    };
     
-    var optionsViews = {
-      seriesBarDistance: 10,
-      classNames: {
-        bar: 'ct-bar ct-azure'
-      },
-      axisX: {
-        showGrid: false
-      }
-    };
-    
-    var responsiveOptionsViews = [
-      ['screen and (max-width: 640px)', {
-        seriesBarDistance: 5,
-        axisX: {
-          labelInterpolationFnc: function (value) {
-            return value[0];
-          }
-        }
-      }]
-    ];
-    
-    //Chartist.Bar('#chartViews', dataViews, optionsViews, responsiveOptionsViews);
-
-    var dataPreferences = {
-        series: [
-            [25, 30, 20, 25]
-        ]
-    };
-    
-    var optionsPreferences = {
-        donut: true,
-        donutWidth: 40,
-        startAngle: 0,
-        total: 100,
-        showLabel: false,
-        axisX: {
-            showGrid: false
-        }
-    };
-
-    Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-    
-    Chartist.Pie('#chartPreferences', {
-      labels: ['62%','32%','6%'],
-      series: [62, 32, 6]
-    });   
 }
 
