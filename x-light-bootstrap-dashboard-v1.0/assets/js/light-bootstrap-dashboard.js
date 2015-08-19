a779aea5e4b78b87ce5c709c23066b041dd367d2
Alex Paduraru <axelut@me.com>
2015-08-19 15:49:30 +0300

    small changes

--- x-light-bootstrap-dashboard-v1.0/assets/js/light-bootstrap-dashboard.js
+++ x-light-bootstrap-dashboard-v1.0/assets/js/light-bootstrap-dashboard.js
@@ -53,8 +53,6 @@ lbd = {
         $sidebar = $('.sidebar');
         image_src = $sidebar.data('image');
         
-        $sidebar.removeAttr('data-image');
-        
         if(image_src !== undefined){
             sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
             $sidebar.append(sidebar_container);
