b50f0096b03ba8a7c4365b598a4dc786f562093c
Dragos Ct <dragosct10@gmail.com>
2016-06-08 17:54:26 +0300

    card-header and notifications-js

--- assets/js/bootstrap-notify.js
+++ assets/js/bootstrap-notify.js
@@ -1,10 +1,10 @@
 /*
-    
-    
+
+
 
      Creative Tim Modifications
-     
-     Lines: 239, 240 was changed from top: 5px to top: 50% and we added margin-top: -13px. In this way the close button will be aligned vertically 
+
+     Lines: 239, 240 was changed from top: 5px to top: 50% and we added margin-top: -13px. In this way the close button will be aligned vertically
      Line:242 - modified when the icon is set, we add the class "alert-with-icon", so there will be enough space for the icon.
 
 
@@ -64,7 +64,7 @@
 		onClose: null,
 		onClosed: null,
 		icon_type: 'class',
-		template: '<div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert"><button type="button" aria-hidden="true" class="close" data-notify="dismiss">&times;</button><span data-notify="icon"></span> <span data-notify="title">{1}</span> <span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div>'
+		template: '<div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert"><button type="button" aria-hidden="true" class="close" data-notify="dismiss">&times;</button><i data-notify="icon" class="material-icons"></i><span data-notify="title">{1}</span> <span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div>'
 	};
 
 	String.format = function () {
@@ -135,7 +135,7 @@
 			this.init();
 		}
 	}
-	
+
 	$.extend(Notify.prototype, {
 		init: function () {
 			var self = this;
@@ -171,7 +171,7 @@
 							case "icon":
 								var $icon = this.$ele.find('[data-notify="icon"]');
 								if (self.settings.icon_type.toLowerCase() === 'class') {
-									$icon.removeClass(self.settings.content.icon).addClass(commands[cmd]);
+									$icon.html(commands[cmd]);
 								} else {
 									if (!$icon.is('img')) {
 										$icon.find('img');
@@ -215,11 +215,11 @@
 			}
 		},
 		setIcon: function () {
-    		
+
     		this.$ele.addClass('alert-with-icon');
-    		
+
 			if (this.settings.icon_type.toLowerCase() === 'class') {
-				this.$ele.find('[data-notify="icon"]').addClass(this.settings.content.icon);
+				this.$ele.find('[data-notify="icon"]').html(this.settings.content.icon);
 			} else {
 				if (this.$ele.find('[data-notify="icon"]').is('img')) {
 					this.$ele.find('[data-notify="icon"]').attr('src', this.settings.content.icon);
