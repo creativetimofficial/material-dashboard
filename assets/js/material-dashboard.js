ffdcfbcebe69b92eb5fb8a767228a0c790fa996d
Dragos Ct <dragosct10@gmail.com>
2016-09-14 17:39:00 +0300

    working on material menu navbar

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -48,7 +48,7 @@ $(window).resize(function(){
     }
 });
 
-mkd = {
+md = {
     misc:{
         navbar_menu_visible: 0
     },
@@ -63,85 +63,16 @@ mkd = {
         }
     },
 
-    initRightMenu: function(){
-         if(!navbar_initialized){
-            $navbar = $('nav').find('.navbar-collapse').first().clone(true);
+    initRightMenu: debounce(function(){
+        $sidebar_wrapper = $('.sidebar-wrapper');
 
-            $sidebar = $('.sidebar');
-            sidebar_color = $sidebar.data('color');
+        if(!navbar_initialized){
+            
+        } else {
 
-            $logo = $sidebar.find('.logo').first();
-            logo_content = $logo[0].outerHTML;
-
-            ul_content = '';
-
-            $navbar.attr('data-color',sidebar_color);
-
-            // add the content from the sidebar to the right menu
-            content_buff = $sidebar.find('.nav').html();
-            ul_content = ul_content + content_buff;
-
-            //add the content from the regular header to the right menu
-            $navbar.children('ul').each(function(){
-                content_buff = $(this).html();
-                ul_content = ul_content + content_buff;
-            });
-
-            ul_content = '<ul class="nav navbar-nav">' + ul_content + '</ul>';
-
-            navbar_content = logo_content + ul_content;
-
-            $navbar.html(navbar_content);
-
-            $('body').append($navbar);
-
-            background_image = $sidebar.data('image');
-            if(background_image != undefined){
-                $navbar.css('background',"url('" + background_image + "')")
-                       .removeAttr('data-nav-image')
-                       .addClass('has-image');
-            }
-
-
-             $toggle = $('.navbar-toggle');
-
-             $navbar.find('a').removeClass('btn btn-round btn-default');
-             $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
-             $navbar.find('button').addClass('btn-simple btn-block');
-
-             $toggle.click(function (){
-                if(mkd.misc.navbar_menu_visible == 1) {
-                    $('html').removeClass('nav-open');
-                    mkd.misc.navbar_menu_visible = 0;
-                    $('#bodyClick').remove();
-                     setTimeout(function(){
-                        $toggle.removeClass('toggled');
-                     }, 400);
-
-                } else {
-                    setTimeout(function(){
-                        $toggle.addClass('toggled');
-                    }, 430);
-
-                    div = '<div id="bodyClick"></div>';
-                    $(div).appendTo("body").click(function() {
-                        $('html').removeClass('nav-open');
-                        mkd.misc.navbar_menu_visible = 0;
-                        $('#bodyClick').remove();
-                         setTimeout(function(){
-                            $toggle.removeClass('toggled');
-                         }, 400);
-                    });
-
-                    $('html').addClass('nav-open');
-                    mkd.misc.navbar_menu_visible = 1;
-
-                }
-            });
-            navbar_initialized = true;
         }
 
-    }
+    }, 500);
 }
 
 
