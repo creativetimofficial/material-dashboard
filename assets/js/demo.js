8775c4b22276df1c9995a2f7f415a63a641fbf3d
Axelut <axelut@me.com>
2016-09-20 16:25:00 +0300

    small fixes

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -229,7 +229,7 @@ demo = {
 
     	$.notify({
         	icon: "add_alert",
-        	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+        	message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
 
         },{
             type: type[color],
