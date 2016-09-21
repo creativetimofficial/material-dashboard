1ffad2c032ea745b7f220eada481df595923668f
Dragos Ct <dragosct10@gmail.com>
2016-09-21 12:43:53 +0300

    changes to icons page + erased js from tutorial-components +

--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -67,7 +67,6 @@ $(document).ready(function(){
     //  Activate the tooltips
     $('[rel="tooltip"]').tooltip();
 
-
     //      Activate the switches with icons
     if($('.switch').length != 0){
         $('.switch')['bootstrapSwitch']();
