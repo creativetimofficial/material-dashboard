a0a8c3781ccf717fc4b0850b43892a3d7e3246e3
shababhsiddique <shabab.h.siddique@gmail.com>
2018-02-24 19:53:44 +0600

    changed file formatting to make merge easy, - fallback to font weight for body and navbar

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,22 +1,22 @@
-type = ['', 'info', 'success', 'warning', 'danger'];
+type = ['','info','success','warning','danger'];
 
 
 demo = {
-    initPickColor: function () {
-        $('.pick-class-label').click(function () {
+    initPickColor: function(){
+        $('.pick-class-label').click(function(){
             var new_class = $(this).attr('new-class');
             var old_class = $('#display-buttons').attr('data-class');
             var display_div = $('#display-buttons');
-            if (display_div.length) {
-                var display_buttons = display_div.find('.btn');
-                display_buttons.removeClass(old_class);
-                display_buttons.addClass(new_class);
-                display_div.attr('data-class', new_class);
+            if(display_div.length) {
+            var display_buttons = display_div.find('.btn');
+            display_buttons.removeClass(old_class);
+            display_buttons.addClass(new_class);
+            display_div.attr('data-class', new_class);
             }
         });
     },
 
-    initFormExtendedDatetimepickers: function () {
+    initFormExtendedDatetimepickers: function(){
         $('.datetimepicker').datetimepicker({
             icons: {
                 time: "fa fa-clock-o",
@@ -29,10 +29,10 @@ demo = {
                 clear: 'fa fa-trash',
                 close: 'fa fa-remove'
             }
-        });
+         });
     },
 
-    initDocumentationCharts: function () {
+    initDocumentationCharts: function(){
         /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
 
         dataDailySalesChart = {
@@ -48,7 +48,7 @@ demo = {
             }),
             low: 0,
             high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
         }
 
         var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
@@ -56,7 +56,7 @@ demo = {
         md.startAnimationForLineChart(dailySalesChart);
     },
 
-    initDashboardPageCharts: function () {
+    initDashboardPageCharts: function(){
 
         /* ----------==========     Daily Sales Chart initialization    ==========---------- */
 
@@ -73,7 +73,7 @@ demo = {
             }),
             low: 0,
             high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
         }
 
         var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
@@ -97,7 +97,7 @@ demo = {
             }),
             low: 0,
             high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {top: 0, right: 0, bottom: 0, left: 0}
+            chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
         }
 
         var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
@@ -110,11 +110,11 @@ demo = {
         /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
 
         var dataEmailsSubscriptionChart = {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-                [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
 
-            ]
+          ]
         };
         var optionsEmailsSubscriptionChart = {
             axisX: {
@@ -122,17 +122,17 @@ demo = {
             },
             low: 0,
             high: 1000,
-            chartPadding: {top: 0, right: 5, bottom: 0, left: 0}
+            chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
         };
         var responsiveOptions = [
-            ['screen and (max-width: 640px)', {
-                    seriesBarDistance: 5,
-                    axisX: {
-                        labelInterpolationFnc: function (value) {
-                            return value[0];
-                        }
-                    }
-                }]
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
         ];
         var emailsSubscriptionChart = Chartist.Bar('#emailsSubscriptionChart', dataEmailsSubscriptionChart, optionsEmailsSubscriptionChart, responsiveOptions);
 
@@ -141,109 +141,25 @@ demo = {
 
     },
 
-    initGoogleMaps: function () {
+    initGoogleMaps: function(){
         var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
         var mapOptions = {
-            zoom: 13,
-            center: myLatlng,
-            scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-            styles: [{
-                    "featureType": "water",
-                    "stylers": [{
-                            "saturation": 43
-                        }, {
-                            "lightness": -11
-                        }, {
-                            "hue": "#0088ff"
-                        }]
-                }, {
-                    "featureType": "road",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                            "hue": "#ff0000"
-                        }, {
-                            "saturation": -100
-                        }, {
-                            "lightness": 99
-                        }]
-                }, {
-                    "featureType": "road",
-                    "elementType": "geometry.stroke",
-                    "stylers": [{
-                            "color": "#808080"
-                        }, {
-                            "lightness": 54
-                        }]
-                }, {
-                    "featureType": "landscape.man_made",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                            "color": "#ece2d9"
-                        }]
-                }, {
-                    "featureType": "poi.park",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                            "color": "#ccdca1"
-                        }]
-                }, {
-                    "featureType": "road",
-                    "elementType": "labels.text.fill",
-                    "stylers": [{
-                            "color": "#767676"
-                        }]
-                }, {
-                    "featureType": "road",
-                    "elementType": "labels.text.stroke",
-                    "stylers": [{
-                            "color": "#ffffff"
-                        }]
-                }, {
-                    "featureType": "poi",
-                    "stylers": [{
-                            "visibility": "off"
-                        }]
-                }, {
-                    "featureType": "landscape.natural",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                            "visibility": "on"
-                        }, {
-                            "color": "#b8cb93"
-                        }]
-                }, {
-                    "featureType": "poi.park",
-                    "stylers": [{
-                            "visibility": "on"
-                        }]
-                }, {
-                    "featureType": "poi.sports_complex",
-                    "stylers": [{
-                            "visibility": "on"
-                        }]
-                }, {
-                    "featureType": "poi.medical",
-                    "stylers": [{
-                            "visibility": "on"
-                        }]
-                }, {
-                    "featureType": "poi.business",
-                    "stylers": [{
-                            "visibility": "simplified"
-                        }]
-                }]
+          zoom: 13,
+          center: myLatlng,
+          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+          styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
 
         }
         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
         var marker = new google.maps.Marker({
             position: myLatlng,
-            title: "Hello World!"
+            title:"Hello World!"
         });
 
         // To add the marker to the map, call setMap();
         marker.setMap(map);
-    },
+    },	
 
     showNotification: function (from, align) {
         color = Math.floor((Math.random() * 4) + 1);
@@ -261,7 +177,5 @@ demo = {
             }
         });
     }
-
-
 
 }
