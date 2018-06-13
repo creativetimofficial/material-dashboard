2539cc5252c46c882358819c587744c4d7d3e8dd
Stefan Moldovan <moldovanste@gmail.com>
2018-06-13 16:43:41 +0300

    small changes

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -232,25 +232,6 @@ demo = {
 
     // To add the marker to the map, call setMap();
     marker.setMap(map);
-  },
-
-  showNotification: function(from, align) {
-    type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
-
-    color = Math.floor((Math.random() * 6) + 1);
-
-    $.notify({
-      icon: "add_alert",
-      message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
-
-    }, {
-      type: type[color],
-      timer: 3000,
-      placement: {
-        from: from,
-        align: align
-      }
-    });
   }
 
 }
--- assets/js/material-dashboard.js
+++ assets/js/material-dashboard.js
@@ -164,6 +164,25 @@ md = {
     }
   },
 
+  showNotification: function(from, align) {
+    type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
+
+    color = Math.floor((Math.random() * 6) + 1);
+
+    $.notify({
+      icon: "add_alert",
+      message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
+
+    }, {
+      type: type[color],
+      timer: 3000,
+      placement: {
+        from: from,
+        align: align
+      }
+    });
+  },
+
   initFormExtendedDatetimepickers: function() {
     $('.datetimepicker').datetimepicker({
       icons: {
